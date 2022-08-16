65dee091274f5107e1109fb855de0bd03ea4d668
Ashwini Namboothiri <60285499+AshwiniNam@users.noreply.github.com>
2022-08-16 23:53:55 +0530

    Login page

--- app/src/main/java/com/example/register/MainActivity.java
+++ app/src/main/java/com/example/register/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.register;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.icu.text.UnicodeSetSpanner;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView username = (TextView)findViewById(R.id.username);
+        TextView password = (TextView)findViewById(R.id.password);
+        MaterialButton loginbutton = (MaterialButton)findViewById(R.id.loginbutton);
+
+        loginbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin"))
+                {
+                    Toast.makeText(MainActivity.this,"LOGIN SUCCESSFUL",Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(MainActivity.this,"LOGIN FAILED",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
